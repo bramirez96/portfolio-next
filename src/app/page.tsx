a5f47226223e0ba500b731e8512d3de67f4cc4f6
bramirez96 <bran.ramirez.don@gmail.com>
2024-04-29 14:08:19 -0700

    Remove the mock cardwall nonsense and commit all code so I can move on to auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,10 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const files = [
-  "https://utfs.io/f/6a4dc840-7e04-4584-9f69-bb7e53088b96-mes7ti.svg",
-  "https://utfs.io/f/b778596f-2c63-4671-874d-5d105adeaefa-ezf5w8.jpg",
-];
-
-const mockImages = files.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
-const wallData = [
-  ...mockImages,
-  ...mockImages,
-  ...mockImages,
-  ...mockImages,
-  ...mockImages,
-  ...mockImages,
-];
+const files = {
+  coder: "https://utfs.io/f/6a4dc840-7e04-4584-9f69-bb7e53088b96-mes7ti.svg",
+  selfie: "https://utfs.io/f/b778596f-2c63-4671-874d-5d105adeaefa-ezf5w8.jpg",
+};
 
 export default async function HomePage() {
   const posts = await db.query.projects.findMany();
@@ -31,11 +17,6 @@ export default async function HomePage() {
       <div className="flex flex-wrap gap-4">
         {posts.map((post) => (
           <div key={post.id}>{post.title}</div>
-        ))}
-        {wallData.map((image, index) => (
-          <div key={image.id + "_" + index} className="w-48">
-            <img src={image.url} alt="image" />
-          </div>
         ))}
       </div>
     </main>
