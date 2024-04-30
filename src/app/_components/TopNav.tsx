88240dff31668fa40fb6f66378b2cbf9ef77004f
bramirez96 <bran.ramirez.don@gmail.com>
2024-04-29 18:04:56 -0700

    Subtle flex 😏

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -4,7 +4,7 @@ export function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b-2 border-red-600 p-4 text-xl font-semibold">
       <div>Home</div>
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
         </SignedOut>
